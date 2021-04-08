4c59403520e8e631baaa71612625d6ab215f1a10
Satyam Sehgal <satyam.sehgal@africa.airtel.com>
2021-04-08 21:56:55 +0530

    Initial Commit

---
+++ SwiggyAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiggyAssignment
+//
+//  Created by 13221494 on 08/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
